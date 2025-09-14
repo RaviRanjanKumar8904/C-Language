6bfd05d979c86b1f7e760cf3c030b1d3c9d32e30
Ravi Ranjan Kuamr <raviranjan8904@gmail.com>
2025-09-14 16:12:10 +0530

    Organizing with number.

---
+++ 02_learning.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+
+    // Variable is a container to store values.
+    printf("RULES OF DECLEARING VARIABLE\n\n");
+
+    printf("Variable name must start with alphabet\n");
+    printf("Special Character is not allowed except Underscore(_)\n");
+    printf("No Blank space is allowed.\n");
+    printf("It is case sensetive\n");
+    printf("We should Give meaningfull and readable variable\n");
+
+    printf("\n\n\n");
+
+    // Data Types.
+    printf("DATA TYPES\n\n");
+
+    int a = 1;
+    printf("Size of int is %d", sizeof(a));
+
+    float b = 1.1;
+    printf("\nsize of float is %d", sizeof(b));
+
+    char c = 'k';
+    printf("\nSize of char is %d", sizeof(c));
+
+    return 0;
+}
