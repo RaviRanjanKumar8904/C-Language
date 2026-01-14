332dca13431ca3040b311a3bc87bbd21cf892bde
Ravi Ranjan Kumar <raviranjan8904@gmail.com>
2026-01-14 22:25:08 +0530

    v2

--- Revision/helloWorld.c
+++ Revision/helloWorld.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+
+//Write a program to print "Hello, Ravi!" on the first line and your age (as an integer variable) on the second line.
 int main()
 {
-    printf("Hello World");
+    printf("Hello, Ravi\n");
+    printf("Age 22");
     return 0;
 }
